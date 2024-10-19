5d57b9be167a32291c45eaf043fdd4e7b69057b8
FriendlyGuardian <purplepenguin998@gmail.com>
2024-10-19 16:02:14 -0600

    mid way through comments!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Resources/AprilTagSystem.java
@@ -0,0 +1,124 @@
+package org.firstinspires.ftc.teamcode.Resources;
+
+// huge props to our mentor who also wrote this class in the FIRST SDK, Dryw Wade!
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+// used for figuring out the robots position with the april tags around the field.
+public class AprilTagSystem {
+ 
+  // TODO: tune these values to match the physical robot.
+  
+  // the position of the camera lens relative to the center of the robot
+  private Position cameraPosition = new Position(DistanceUnit.INCH,
+          0, 0, 0, 0);
+  
+  // the orientation of the camera lens relative to the orientation of the robot
+  private YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
+          0, -90, 0, 0);
+  
+  
+  // end of the todo values
+  
+  // the latest detection the processor has found
+  private AprilTagDetection detection;
+  
+  /**
+   * The variable to store our instance of the AprilTag processor.
+   */
+  private AprilTagProcessor aprilTag;
+  
+  /**
+   * The variable to store our instance of the vision portal.
+   */
+  private VisionPortal visionPortal;
+  
+  /**
+   * Initialize the AprilTag processor.
+   * @param hardwareMap the hardware map instance from the first SDK
+   */
+  private AprilTagSystem(HardwareMap hardwareMap) {
+    
+    // Create the AprilTag processor.
+    aprilTag = new AprilTagProcessor.Builder()
+            
+            .setCameraPose(cameraPosition, cameraOrientation).build();
+    
+    // Create the vision portal by using a builder.
+    VisionPortal.Builder builder = new VisionPortal.Builder();
+    
+    builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 2"));
+    
+    builder.addProcessor(aprilTag);
+    
+    // Build the Vision Portal, using the above settings.
+    visionPortal = builder.build();
+    
+  }
+  
+  /**
+   * used to figure out which april tag to use then
+   * calculates the position of the april tags
+   * @param robotXCurrent
+   * @param robotYCurrent
+   */
+  public void findAprilTags(double robotXCurrent, double robotYCurrent) {
+    // gets all of the april tags we can see
+    List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+    
+    // stores the best detection we have found so far
+    AprilTagDetection bestDetection = null;
+    
+    //TODO: handle the case in which there are no visible tags
+    
+    // the idea for choosing the best tag to use it's data
+    for (AprilTagDetection detection : currentDetections) {
+      double RX = robotXCurrent, RY = robotYCurrent;
+      double TX = detection.robotPose.getPosition().x, TY = detection.robotPose.getPosition().y;
+      double BX = bestDetection.robotPose.getPosition().x, BY = bestDetection.robotPose.getPosition().y;
+      
+      
+      if (bestDetection != null) {
+        if (calculateDistance(RX,RY,TX,TY) > calculateDistance(RX,RY,BX,BY)) continue;
+      }
+      
+      this.detection = detection;
+      bestDetection = detection;
+    }
+  }
+  
+  public double calculateDistance(double robotX, double robotY, double projectedX, double projectedY) {
+    return Math.sqrt(Math.pow(robotX - projectedX, 2) + Math.pow(robotY + projectedY, 2));
+  }
+  
+  public int getTagID() {
+    return detection.id;
+  }
+  
+  public double getRobotX() {
+    return detection.robotPose.getPosition().x;
+  }
+  
+  public double getRobotY() {
+    return detection.robotPose.getPosition().y;
+  }
+  
+  public double getRobotZ() {
+    return detection.robotPose.getPosition().z;
+  }
+  
+  public double getRobotYaw() {
+    return detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES);
+  }
+}
+
