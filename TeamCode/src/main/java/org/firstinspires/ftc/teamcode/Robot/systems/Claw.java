e22bdb6252c1232ae266c35441941509905b6f99
JenX1 <jennifer.lane0726@gmail.com>
2024-10-23 19:12:28 -0600

    Added comments to Arm and Claw classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/systems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/systems/Claw.java
@@ -4,9 +4,17 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw {
+    //The servos which the claw uses
     ServoPlus claw1Servo;
     ServoPlus claw2Servo;
 
+    /**
+     * The four positions/states of the claw
+     * open
+     * closed
+     * grabOutside
+     * grabInside
+     */
     public enum ClawPosition{
         open,
         closed,
@@ -14,16 +22,26 @@ public class Claw {
         grabInside,
     }
 
+    /**
+     * Claw constructor
+     * @param hardwareMap Robot hardware map
+     */
     public Claw (HardwareMap hardwareMap){
+        //Mapping/initializing the claw
         claw1Servo = new ServoPlus(hardwareMap.get(Servo.class,"claw1Servo"),
                 180,0,180);
         claw2Servo = new ServoPlus(hardwareMap.get(Servo.class,"claw2Servo"),
                 180,0,180);
     }
+
+    /**
+     * Controls the four positions of the claw
+     * @param position Must be one of the values from {@link org.firstinspires.ftc.teamcode.Robot.systems.Claw.ClawPosition}
+     */
     public void setPosition(ClawPosition position){
         switch (position){
-
             //TODO: un-zero these values
+            //Servo positions for the four claw states
             case open:
                 claw1Servo.setServoPos(0);
                 claw2Servo.setServoPos(0);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/systems/SpampleArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/systems/SpampleArm.java
@@ -5,24 +5,31 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class SpampleArm {
+    //Actuators for the arm
     DcMotor shoulderMotor;
     DcMotor linearSlideMotor;
     ServoPlus wristServo;
     ServoPlus twistServo;
     Claw claw;
 
-    //PPR is Pulses Per Revolution
     //TODO: replace with correct value; calibrated for 312 RPM motor
-    double shoulderPPR = 537.7;
-    double shoulderTicksPerDegrees = shoulderPPR / 360;
+    //Constants for the shoulder
+    double shoulderPulsesPerRevolution = 537.7;
+    double shoulderTicksPerDegrees = shoulderPulsesPerRevolution / 360;
 
-    double linearSlidePPR = 537.7;
+    //Constants for the linear slide
+    double linearSlidePulsesPerRevolution = 537.7;
     double linearSlideRevPerInch = 1;
-    double linearSlideTicksPerInch = linearSlidePPR * linearSlideRevPerInch;
+    double linearSlideTicksPerInch = linearSlidePulsesPerRevolution * linearSlideRevPerInch;
     double linearSlideMaxExtension = 23;
 
+    /**
+     * Arm constructor
+     * @param hardwareMap Robot hardware map
+     */
     public SpampleArm (HardwareMap hardwareMap){
         //TODO: confirm names
+        //Mapping/initializing motors
         shoulderMotor = hardwareMap.get(DcMotor.class,"shoulderMotor");
         shoulderMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         shoulderMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -31,6 +38,7 @@ public class SpampleArm {
         linearSlideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         linearSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
+        //Mapping/initializing servos
         wristServo = new ServoPlus(hardwareMap.get(Servo.class,"wristServo"),
                 180,0,180);
 
@@ -40,6 +48,10 @@ public class SpampleArm {
         claw = new Claw(hardwareMap);
     }
 
+    /**
+     * Controls the shoulder motor
+     * @param angle Angle for shoulder in degrees
+     */
     public void rotateShoulderTo (double angle){
         if (angle < 0) {
             angle = 0;
@@ -50,6 +62,10 @@ public class SpampleArm {
         shoulderMotor.setTargetPosition((int) (angle * shoulderTicksPerDegrees));
     }
 
+    /**
+     * Controls the linear slide
+     * @param inches Distance for extension in inches
+     */
     public void extendTo (double inches){
         if (inches < 0){
             inches = 0;
@@ -60,12 +76,26 @@ public class SpampleArm {
         linearSlideMotor.setTargetPosition((int) (inches * linearSlideTicksPerInch));
     }
 
+    /**
+     * Controls the wrist motor
+     * @param angle Angle for wrist in degrees
+     */
     public void rotateWristTo (double angle){
         wristServo.setServoPos(angle);
     }
+
+    /**
+     * Controls the twist of the wrist
+     * @param angle Angle for twist rotation in degrees
+     */
     public void rotateTwistTo (double angle){
         twistServo.setServoPos(angle);
     }
+
+    /**
+     * Controls the four positions of the claw
+     * @param position Must be one of the values from {@link org.firstinspires.ftc.teamcode.Robot.systems.Claw.ClawPosition}
+     */
     public void setClawPosition (Claw.ClawPosition position){
         claw.setPosition(position);
     }
