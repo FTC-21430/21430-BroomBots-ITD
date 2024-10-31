693b5035ab3c72229ba7cd0ba56ca633b801a3b7
Cata48 <cata.e.fattor@gmail.com>
2024-10-30 20:00:31 -0600

    Inches to Ticks
    Added code to convert inches to ticks to make it easier to code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Systems/Climber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Systems/Climber.java
@@ -4,14 +4,23 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import java.util.Base64;
+
 // This class has the functions for the climbers
 public class Climber {
     // These are the two motors that control the climber and they run in parallel
     DcMotor leftClimberMotor;
     DcMotor rightClimberMotor;
-    Gamepad gamepad1;
     Gamepad gamepad2;
+
+    //THIS IS A PLACEHOLDER. More testing is needed.
+    final private double extended = 9.0;
+
     // This function is always the starting position of the climber at 0,0
+
+    //THESE NUMBERS ARE PLACEHOLDERS! 25.4 is mm per inch|2000 is ticks per revolution|48*Math.PI is
+    // mm per revolution
+    private double ticksPerInches = 25.4*2000/(48 * Math.PI);
 
     // The motors go to their lowest extension
     private void startingPosition(){
@@ -23,10 +32,10 @@ public class Climber {
 
     // The motors extend to the height required to climb
     private void level1Ascent() {
-            if (gamepad2.dpad_right) {
-                leftClimberMotor.setTargetPosition(1);
-                rightClimberMotor.setTargetPosition(1);
-            }
+        if (gamepad2.dpad_right) {
+            leftClimberMotor.setTargetPosition((int) (extended*ticksPerInches));
+            rightClimberMotor.setTargetPosition((int) (extended*ticksPerInches));
+        }
     }
 
     // The motors pull the robot up to the bar
@@ -35,17 +44,15 @@ public class Climber {
             leftClimberMotor.setTargetPosition(0);
             rightClimberMotor.setTargetPosition(0);
         }
-
     }
 
     // The motors extend to the highest bar and then pull it up
     private void level3Ascent(){
         if (gamepad2.dpad_up){
-            leftClimberMotor.setTargetPosition(1);
-            rightClimberMotor.setTargetPosition(1);
+            leftClimberMotor.setTargetPosition((int) (extended*ticksPerInches));
+            rightClimberMotor.setTargetPosition((int) (extended*ticksPerInches));
             leftClimberMotor.setTargetPosition(0);
             rightClimberMotor.setTargetPosition(0);
         }
-
     }
 }
