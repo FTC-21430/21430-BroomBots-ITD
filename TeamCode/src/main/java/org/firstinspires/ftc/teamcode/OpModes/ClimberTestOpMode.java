d93d476e06fab320f6b0511584b139ae6d55bbb4
Cata48 <cata.e.fattor@gmail.com>
2024-11-16 10:46:14 -0700

    Removed If-Statements from Climb Assent
    We plan on putting the if-statements in tele-op so that we can have all of the tele-op logic in the same place.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ClimberTestOpMode.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.Robot.Systems.Climber;
+
+public class ClimberTestOpMode extends BaseTeleOp{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Climber climber = new Climber();
+        Gamepad gamepad2 = new Gamepad();
+        if(gamepad2.dpad_left){
+            climber.level1Ascent();
+        }
+        if(gamepad2.dpad_up){
+            climber.level2Ascent();
+        }
+        if(gamepad2.dpad_right){
+            climber.level3Ascent();
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Systems/Climber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Systems/Climber.java
@@ -11,7 +11,10 @@ public class Climber {
     // These are the two motors that control the climber and they run in parallel
     DcMotor leftClimberMotor;
     DcMotor rightClimberMotor;
-    Gamepad gamepad2;
+
+    public Climber(){
+        startingPosition();
+    }
 
     //THIS IS A PLACEHOLDER. More testing is needed.
     final private double extended = 9.0;
@@ -23,36 +26,28 @@ public class Climber {
     private double ticksPerInches = 25.4*2000/(48 * Math.PI);
 
     // The motors go to their lowest extension
-    private void startingPosition(){
-        if (gamepad2.dpad_down){
+    public void startingPosition(){
         leftClimberMotor.setTargetPosition(0);
         rightClimberMotor.setTargetPosition(0);
-        }
     }
 
     // The motors extend to the height required to climb
-    private void level1Ascent() {
-        if (gamepad2.dpad_right) {
+    public void level1Ascent() {
             leftClimberMotor.setTargetPosition((int) (extended*ticksPerInches));
             rightClimberMotor.setTargetPosition((int) (extended*ticksPerInches));
-        }
     }
 
     // The motors pull the robot up to the bar
-    private void level2Ascent(){
-        if (gamepad2.dpad_left){
+    public void level2Ascent(){
             leftClimberMotor.setTargetPosition(0);
             rightClimberMotor.setTargetPosition(0);
-        }
     }
 
     // The motors extend to the highest bar and then pull it up
-    private void level3Ascent(){
-        if (gamepad2.dpad_up){
+    public void level3Ascent(){
             leftClimberMotor.setTargetPosition((int) (extended*ticksPerInches));
             rightClimberMotor.setTargetPosition((int) (extended*ticksPerInches));
             leftClimberMotor.setTargetPosition(0);
             rightClimberMotor.setTargetPosition(0);
-        }
     }
 }
