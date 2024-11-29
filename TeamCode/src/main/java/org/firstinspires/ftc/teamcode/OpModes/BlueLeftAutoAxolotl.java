b6c4aeaff0da9fa3df3664879a2bde4ed58a615e
FriendlyGuardian <purplepenguin998@gmail.com>
2024-11-29 11:11:23 -0700

    commented out calls to deprecated functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/BlueLeftAutoAxolotl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/BlueLeftAutoAxolotl.java
@@ -10,20 +10,20 @@ public class BlueLeftAutoAxolotl extends BaseAuto {
   //None of the values for RunToPoint Functions are correct
         @Override
         public void runOpMode() throws InterruptedException {
-            AutonomousFunctions.WaitFunction();
-            RunToPoint(48, 10, 0.2);
-            robot.arm.highChamber();
-            RunToPoint(24, 15,0.1);
-
-            for (int i = 0; i<3; i++)
-            {
-                robot.arm.grabSpample();
-                RunToPoint(-12,-34,0.3);
-                robot.arm.highBasket();
-                RunToPoint(12,34, 0.3);
-
-            }
-            RunToPoint(24, 15, 0.4);
+//            AutonomousFunctions.WaitFunction();
+//            RunToPoint(48, 10, 0.2);
+//            robot.arm.highChamber();
+//            RunToPoint(24, 15,0.1);
+//
+//            for (int i = 0; i<3; i++)
+//            {
+//                robot.arm.grabSpample();
+//                RunToPoint(-12,-34,0.3);
+//                robot.arm.highBasket();
+//                RunToPoint(12,34, 0.3);
+//
+//            }
+//            RunToPoint(24, 15, 0.4);
             //TODO Add Climb Level 1 Assent funciton
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/BlueRightAutoAxolotl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/BlueRightAutoAxolotl.java
@@ -5,22 +5,22 @@ public class BlueRightAutoAxolotl extends BaseAuto {
     @Override
     public void runOpMode() throws InterruptedException {
         
-        // TODO Update all of these runto point functions to be the updated AutoMoveTo function in robot.java
+        // TODO Update all of these runToPoint functions to be the updated AutoMoveTo function in robot.java
         
-        robot.autoMoveTo(48, 12,0,2);
-        robot.arm.highChamber();
-        RunToPoint(48,12, 0.4);
-        robot.arm.grabSpample();
-        RunToPoint(48, 8, 0.3);
-        robot.arm.dropOff();
-        RunToPoint(50, 12, 0.3);
-        robot.arm.grabSpample();
-        RunToPoint(48,8,0.3);
-        robot.arm.dropOff();
-        //add grab spicement
-        RunToPoint(42,24, 0.2);
-        robot.arm.highChamber();
-        RunToPoint(28,-10,0.2);
+//        robot.autoMoveTo(48, 12,0,2);
+//        robot.arm.highChamber();
+//        RunToPoint(48,12, 0.4);
+//        robot.arm.grabSpample();
+//        RunToPoint(48, 8, 0.3);
+//        robot.arm.dropOff();
+//        RunToPoint(50, 12, 0.3);
+//        robot.arm.grabSpample();
+//        RunToPoint(48,8,0.3);
+//        robot.arm.dropOff();
+//        //add grab spicement
+//        RunToPoint(42,24, 0.2);
+//        robot.arm.highChamber();
+//        RunToPoint(28,-10,0.2);
     }
 
 }
